09325e849d4e3afbb80c9271b7cb877bd5380133
m3yevn <mmm.myintmyat@gmail.com>
2021-08-01 14:28:40 +0800

    remove: mobile responsiveness support. disable screen if mobile

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <Header />
-  <router-view />
+  <Header v-if="largeScreen" />
+  <router-view v-if="largeScreen" />
+  <MobileMessage v-if="smallScreen" />
 </template>
 
 <script>
 import { store } from "./utils/Data.store";
 import Header from "./components/Header.vue";
+import MobileMessage from "./components/MobileMessage.vue";
 
 export default {
+  data() {
+    return {
+      smallScreen: window.matchMedia("(max-width:1024px)").matches,
+      largeScreen: window.matchMedia("(min-width:1024px)").matches,
+    };
+  },
   components: {
     Header,
+    MobileMessage,
   },
   async mounted() {
     await store.openDatabase();
---
+++ src/components/MobileMessage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <MobileAlert />
+    <h1>Kanbaneon cannot be used on Mobile Devices.</h1>
+    <h5>Please open it on PC browser.</h5>
+  </div>
+</template>
+
+<script>
+import MobileAlert from "../assets/MobileAlert.vue";
+export default {
+  components: {
+    MobileAlert,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  background: #42b883;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+h1 {
+  color: white;
+  font-size: 46px;
+}
+h5 {
+  color: white;
+  font-size: 16px;
+}
+a {
+  text-decoration: underline;
+  color: #35495e;
+  font-weight: bold;
+}
+</style>
